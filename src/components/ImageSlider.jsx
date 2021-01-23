8549d07e4a375aea9bb2fcc5d295856cf971bb40
denmercs <denmercs@gmail.com>
2021-01-23 13:19:55 -0600

    modified layout in product details component

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -5,16 +5,21 @@ const ImageSlider = ({ images }) => {
   const [imagePreview, setImagePreview] = useState(images[0]);
 
   return (
-    <>
+    <div className="image">
       <Image.PreviewGroup>
-        <Image width={200} preview={true} src={imagePreview} />
+        <Image width={200} preview={true} src={imagePreview} />.
+        <div className="image-preview">
+          {images.map((image) => {
+            if (image !== imagePreview) return <Image src={image} />;
+          })}
+        </div>
       </Image.PreviewGroup>
       <div className="image-container">
         {images.map((image) => (
           <img src={image} onMouseEnter={() => setImagePreview(image)} />
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProductId } from "../redux/actions/productsAction";
 import ImageSlider from "./ImageSlider";
+import { Button } from "antd";
 
 const ProductDetails = (props) => {
   let id = props.match.params.id;
@@ -15,10 +16,14 @@ const ProductDetails = (props) => {
   return (
     <div className="details">
       <ImageSlider key={item._id} images={item.image} />
-      <h3>{item.name}</h3>
-      <h3>Price: ${item.price}</h3>
-      <p>Brand: {item.brand}</p>
-      <p>Description: {item.description}</p>
+
+      <div className="details-description">
+        <h2>{item.name}</h2>
+        <h3>Price: ${item.price}</h3>
+        <p>Brand: {item.brand}</p>
+        <p>Description: {item.description}</p>
+        <Button type="primary">Add to Cart</Button>
+      </div>
     </div>
   );
 };
