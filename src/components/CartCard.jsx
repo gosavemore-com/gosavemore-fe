5124f982b5a3840f8918c62cbb9328d635494e4e
denmercs <denmercs@gmail.com>
2021-01-27 17:43:21 -0600

    fixing bug on select option of the shoppping cart

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Select, Form } from "antd";
 import { addToCart, removeFromCart } from "../redux/actions/cartAction";
 import { AiFillCloseCircle } from "react-icons/ai";
 
 const CartCard = ({ item }) => {
   const dispatch = useDispatch();
+
   const onChangeHandler = (values) => {
     dispatch(addToCart(item.product, values));
   };
-
   const onClickHandler = () => {
     dispatch(removeFromCart(item.product));
   };
+
   return (
     <div className="cartCard">
       <img src={item.image[0]} alt={item.name} className="cartCard-image" />
@@ -20,7 +21,7 @@ const CartCard = ({ item }) => {
         <h3>Name: {item.name}</h3>
         <p>Price: ${item.price}</p>
         <Form.Item className="cartCard-select">
-          <Select defaultValue={item.quantity} onChange={onChangeHandler}>
+          <Select value={item.quantity} onChange={onChangeHandler}>
             {[...Array(item.countInStock).keys()].map((x) => (
               <Select.Option value={x + 1}>{x + 1}</Select.Option>
             ))}
